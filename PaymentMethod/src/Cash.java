4494867c388a9f919a72a1514e4017629927ddcc
LjBulatao <115807743+LuisBulatao@users.noreply.github.com>
2024-04-29 12:53:29 +0800

    Add files via upload

---
+++ PaymentMethod/src/Cash.java
@@ -0,0 +1,7 @@
+    public class Cash implements PaymentMethod {
+        @Override
+        public void pay(double amount) {
+            System.out.println("You are paying in Cash");
+        }
+    }
+
